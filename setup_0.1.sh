f67940b9494bdd08bef289df9ed756d0fd4b06d9
alexandrma <83217un@gmail.com>
2016-11-05 17:26:32 +0300

    beg

---
+++ setup_0.1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Alexx2000$
+sudo apt-add-repository ppa:jtaylor/keepass
+sudo apt-get update
+
+sudo apt-get install doublecmd-gtk
+sudo apt-get install keepass2
+
